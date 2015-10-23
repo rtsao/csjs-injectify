cba1ce0ae8a6ae1a622a800b8616d84c8a54d0b5
Ryan Tsao <rtsao@uber.com>
2015-10-23 03:08:13 -0700

    more robust implementation

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-"use strict";
+'use strict';
 
-var transformTools = require("browserify-transform-tools");
+var acorn = require('acorn');
+var falafel = require('falafel');
 
-module.exports = transformTools.makeRequireTransform("requireTransform",
-  {evaluateArguments: true},
-  function(args, opts, cb) {
-    if (args[0] === "csjs") {
-      return cb(null, "require('csjs-injectify/csjs-inject')");
-    } else {
-      return cb();
+var regex = /(['"`])csjs\1/;
+
+module.exports = function (file) {
+  var output = through(function(buf, enc, next) {
+    var source = buf.toString('utf8');
+
+    try {
+      var injectified = falafel(source, {
+        parser: acorn,
+        ecmaVersion: 6,
+        sourceType: 'module'
+      }, walk);
+    } catch (err) {
+      return error(err)
+    }
+
+    this.push(injectified);
+    next();
+  });
+
+  function error(msg) {
+    var err = typeof msg === 'string' ? new Error(msg) : msg;
+    output.emit('error', err);
+  }
+
+  return output;
+};
+
+function walk(node){
+  if (node.type === 'ImportDeclaration') {
+    node.update(node.source().replace(regex, '$1bcsjs-inject$1'))
+  } else if (isRequire(node)) {
+    if (node.arguments[0].value === 'csjs') {
+      var quote = node.arguments[0].raw[0][0];
+      node.arguments[0].update(quote + 'csjs-inject' + quote);
     }
   }
-);
+}
+
+function isRequire(node) {
+  return node.callee &&
+    node.type === 'CallExpression' &&
+    node.callee.type === 'Identifier' &&
+    node.callee.name === 'require';
+}
